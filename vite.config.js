b274cbe346370c8bff481d344e116831475688f4
Cameron Ketcham <cketcham@gmail.com>
2023-10-19 09:48:29 -0400

    Add sentry source maps

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "cketcham-4927267cc",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
