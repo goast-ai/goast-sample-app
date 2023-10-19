2ae2607d8f192ca05f2515ed9673005265944b6a
Cameron Ketcham <cketcham@gmail.com>
2023-10-19 09:43:56 -0400

    Add sentry integration

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ A sample react app with some errors
 ## Run this sample
 
 If you want to see the crash in action, you can run this sample yourself by installing the dependencies with `npm install` and then running `npm run dev`. 
+
+## Sentry integration
+
+If you want crashes to go to your sentry integration add a `.env.local` file with your DSN. Something like this: `VITE_SENTRY_DSN="<your dsn>"`
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import * as Sentry from "@sentry/react";
 import App from './App.jsx'
 import './index.css'
+
+if (import.meta.env.VITE_SENTRY_DSN) {
+  Sentry.init({
+    dsn: import.meta.VITE_SENTRY_DSN,
+    integrations: [
+      new Sentry.BrowserTracing({
+        // See docs for support of different versions of variation of react router
+        // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+        routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+          React.useEffect,
+          React.useLocation,
+          React.useNavigationType,
+          React.createRoutesFromChildren,
+          React.matchRoutes
+        ),
+      }),
+      new Sentry.Replay()
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    tracesSampleRate: 1.0,
+
+    // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
