8b8633508c7bd26b6fb47735428e343783946ce8
goast <goastie@goast.ai>
2024-01-04 19:05:19 +0000

    Fix for Manual Fix

--- react/src/DisplayUser.jsx
+++ react/src/DisplayUser.jsx
@@ -3,10 +3,16 @@ import { useState } from 'react';
 function DisplayUser() {
   const [username, setUsername] = useState({first: 'John', last: 'Smith'});
 
-  return (
-    <>
-      <div>Firstname: {username.first}</div>
-      <div>Lastname: {username.last}</div>
+6:  return (
+7:    <>
+8:      {username ? (
+9:        <>
+10:          <div>Firstname: {username.first}</div>
+11:          <div>Lastname: {username.last}</div>
+12:        </>
+13:      ) : (
+14:        <div>No user selected</div>
+15:      )}
       <button onClick={() => setUsername(null)}>Clear Name</button>
     </>
   );
